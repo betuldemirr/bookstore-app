59e3248e7cf512f490581dc630a8d8b099c5d410
betuldemirr <betuldemir.007@gmail.com>
2024-05-02 05:45:31 +0300

    api fetch price and bookDetail fixes

--- src/components/BookDetail.js
+++ src/components/BookDetail.js
@@ -1,44 +1,62 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Card, Modal, Button } from 'react-bootstrap';
+import { getBookId } from '../services/api';
 
 const BookDetail = ({ book }) => {
-  const [showModal, setShowModal] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [price, setPrice] = useState(null);
 
-  const handleShowModal = () => {
-    setShowModal(true);
-  };
+    useEffect(() => {
+        fetchPrice();
+    });
 
-  const handleCloseModal = () => {
-    setShowModal(false);
-  };
+    const fetchPrice = async () => {
+        const bookDetails = await getBookId(book.id);
+        if (bookDetails && bookDetails.saleInfo && bookDetails.saleInfo.listPrice && bookDetails.saleInfo.listPrice.amount) {
+            setPrice(bookDetails.saleInfo.listPrice.amount);
+        } else {
+            console.error('Price data not available.');
+        }
+    };
 
-  return (
-    <>
-      <Card className="h-100" onClick={handleShowModal}>
-        {book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail ? (
-          <Card.Img variant="top" src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
-        ) : (
-          <div className="d-flex justify-content-center align-items-center h-100">No Picture Available</div>
-        )}
-        <Card.Body>
-          <Card.Title>{book.volumeInfo.title}</Card.Title>
-        </Card.Body>
-      </Card>
-      
-      <Modal show={showModal} onHide={handleCloseModal}>
-        <Modal.Header closeButton>
-          <Modal.Title>{book.volumeInfo.title}</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <p><strong>Authors:</strong> {book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Unknown'}</p>
-          <p><strong>Description:</strong> {book.volumeInfo.description ? book.volumeInfo.description : 'No description available'}</p>
-        </Modal.Body>
-        <Modal.Footer>
-          <Button variant="secondary" onClick={handleCloseModal}>Close</Button>
-        </Modal.Footer>
-      </Modal>
-    </>
-  );
+    const handleShowModal = () => {
+        setShowModal(true);
+    };
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
+
+    return (
+        <>
+            <Card className="h-100" >
+                <Card.Body onClick={handleShowModal}>
+                    {book.volumeInfo.imageLinks && book.volumeInfo.imageLinks.thumbnail ? (
+                        <Card.Img variant="top" src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />
+                    ) : (
+                        <div className="d-flex justify-content-center align-items-center h-100">No Picture Available</div>
+                    )}
+                    <Card.Title>{book.volumeInfo.title}</Card.Title>
+                    {price && <p>Price: {price}</p>}
+                </Card.Body>
+                <Card.Footer>
+                    <Button variant="primary">Add to Cart</Button>
+                </Card.Footer>
+
+            </Card>
+            <Modal show={showModal} onHide={handleCloseModal}>
+                <Modal.Header closeButton>
+                <Modal.Title>{book.volumeInfo.title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <p><strong>Authors:</strong> {book.volumeInfo.authors ? book.volumeInfo.authors.join(', ') : 'Unknown'}</p>
+                    <p><strong>Description:</strong> {book.volumeInfo.description ? book.volumeInfo.description : 'No description available'}</p>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleCloseModal}>Close</Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    );
 };
 
 export default BookDetail;
--- src/services/api.js
+++ src/services/api.js
@@ -2,15 +2,22 @@ import axios from 'axios';
 
 const API_KEY = 'AIzaSyAlLHP3hLyscu9NfakV2BXQiCQ4OyAOE4g';
 
-const api = async (searchTerm) => {
-    try {
-        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${API_KEY}`);
-        console.log(response.data);
-        return response.data.items;
-    } catch (error) {
-        console.error('Error fetching data from the server:', error);
-        return [];
-    }
+export const getBookId = async (bookId) => {
+  try {
+    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes/${bookId}?key=${API_KEY}`);
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching book details:', error);
+    return null;
+  }
 };
 
-export default api;
+export const searchBooks = async (searchTerm) => {
+  try {
+    const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${searchTerm}&key=${API_KEY}`);
+    return response.data.items;
+  } catch (error) {
+    console.error('Error fetching data from the server:', error);
+    return [];
+  }
+};
